0c89ca2794893ef9aa3d4c9e5b51f337c1eedebe
zerogvt <gkoumplias@gmail.com>
2020-11-23 17:50:00 +0200

    init

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            displayBio: true
-        };
+    state = {
+        displayBio: true
     }
+    toggleBio = () => {
+        console.log(this)
+        this.setState({
+            displayBio: !this.state.displayBio
+        })
+    }
+
     render() {
-        return(
+        return (
             <div>
                 <h1>Hello!</h1>
                 <p>
                     My name is Vasilis and I am a software engineer.
                 </p>
+                <p>
+                    I'm always looking forward to working on 
+                    challenging and meaningfull projects.
+                </p>
+                <p>
+                    Thanks for dropping by.
+                </p>
+                <p>
                 {
                     this.state.displayBio ?
                     (
@@ -23,16 +35,13 @@ class App extends Component {
                             <p>My favorite language is Python but I also love working with JS and Golang.</p>
                             <p>I also love/need learning new technologies.</p>
                             <p>Besides Software Engineering I also like trekking and sailing.</p>
+                            <button onClick={this.toggleBio}> Less </button>
                         </div>
                     ) :
-                    null
+                    <div>
+                    <button onClick={this.toggleBio}> More </button>
+                    </div>
                 }
-                <p>
-                    I'm always looking forward to working on 
-                    challenging and meaningfull projects.
-                </p>
-                <p>
-                    Thanks for dropping by.
                 </p>
             </div>
         )
